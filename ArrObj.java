2e210fdacf4c16cf8c147f40f459630ded7c4def
Chirag <chiragnabhoya2506@gmail.com>
2025-03-03 23:18:17 +0100

    add ArrObj.java

---
+++ ArrObj.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+class Emp
+{
+	int eid;
+	String ename;
+
+	void set_data(int id,String nm)
+	{
+		eid=id;
+		ename=nm;
+	}
+	void display()
+	{
+		System.out.println("\t Emp ID: "+eid);
+		System.out.println("\t Emp Name: "ename);
+	}
+	
+}
+
+class MyArrObj
+{
+	public static void main("")
+}
