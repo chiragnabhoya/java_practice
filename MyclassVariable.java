ba61167b6ccd7b0d1f113d8cd53c86e430954c8b
Chirag <chiragnabhoya2506@gmail.com>
2025-03-04 22:10:16 +0100

    added MyclassVariable.java

---
+++ MyclassVariable.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+class Emp
+{
+	int eid;
+	String name;
+
+	void display()
+	{
+		System.out.print("\n\t Employee ID: "+eid);
+		System.out.print("\n\t Employee ID: "+ename);
+	}
+}
+
+class MyclassVariable 
+{
+	public static void main(Strng args[])
+	{
+		Emp e = new Emp();
+
+		eid= 101;
+		ename="Ankit";
+
+		e.display;
+	}
+}
