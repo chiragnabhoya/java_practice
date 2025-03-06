ca626fb21ee3c8bf9080b15cdcbbed1f83446beb
Chirag <chiragnabhoya2506@gmail.com>
2025-03-06 21:39:21 +0100

    added MyStaticvar.java

---
+++ MyStaticvar.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+class Emp
+{
+	int eid;
+	String ename;
+	static String ecompany;
+
+	Emp(int id ,String nm)
+	{
+		eid=id;
+		ename=nm;
+	}
+
+	void display()
+	{
+		System.out.println("\n\t EMP ID:"+eid+"\t EMP_NAME:"+ename+"\t EMP_COMPONY"+ecompany);
+
+	}
+} 
+
+
+class MyStaticvar
+{
+	public static void main(String args[])
+	{
+		Emp.ecompany="NOVA";
+
+		Emp e1 = new Emp(101,"ankit");
+		Emp e2 = new Emp(102,"Nishant");
+
+
+		Emp.ecompony="sutex";
+
+		e1.display();
+		e2.display();
+
+
+	}
+}
