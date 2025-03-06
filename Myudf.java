76c044ea7177183d67d6aeaea72dc27a041486b8
Chirag <chiragnabhoya2506@gmail.com>
2025-03-06 21:42:20 +0100

    added Myudf.java

---
+++ Myudf.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+class Myudf
+{
+	//simple function
+	public static void demo1()
+	{	
+		System.out.println("\n\t simple function call...");
+	}
+	
+	//function with argument
+	public static void demo2(int no1,int no2)
+	{
+		System.out.println("\n\t function argument as array..."+ans[0] 
+h);
+	}
+
+	public static void main(String args[])
+	{
+		demo1();
+		demo2(100,200);
+
+		
+	}
+}
